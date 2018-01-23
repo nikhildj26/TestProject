0571f1ac1b980f9993f2f419d0b4857f1e6f6c4e
DELL <DELL@DELL-PC>
2018-01-23 14:41:47 +0530

    Version 1.0

---
+++ com.maven.seleniumjava/src/test/java/com/maven/seleniumjava/SeleniumTestNG.java
@@ -0,0 +1,15 @@
+package com.maven.seleniumjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTestNG {
+	
+	@Test
+	
+	public void firstMethod() {
+		
+		System.out.println("Welcome to Maven world");
+		
+	}
+	
+}
