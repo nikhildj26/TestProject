69cd9ec5a41478608f758c4264595e626576b572
nikhildj26 <nikhiljaltare@gmail.com>
2018-01-31 16:10:36 +0530

    Version 1.2

--- SeleniumDemo/run.bat
+++ SeleniumDemo/run.bat
@@ -1,4 +1,4 @@
 echo off
 cd C:\Users\DELL\git\TestProject\SeleniumDemo\
-set classpath=C:\Selenium\WorkSpace\SeleniumDemo\bin;C:\Selenium\WorkSpace\SeleniumDemo\lib\*;C:\Users\DELL\.p2\pool\plugins\org.testng.eclipse_6.11.0.201703011520\lib\*;
+set classpath=C:\Users\DELL\git\TestProject\SeleniumDemo\bin;C:\Users\DELL\git\TestProject\SeleniumDemo\lib\*;C:\Users\DELL\.p2\pool\plugins\org.testng.eclipse_6.11.0.201703011520\lib\*;
 java org.testng.TestNG testng.xml
